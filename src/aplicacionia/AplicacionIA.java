abfcf66540f54c229a6c24ef070c8cf523969d26
mortal555 <martin.tepostillo@alumnos.udg.mx>
2018-09-14 23:31:18 -0500

    Primer commit: Se han hecho la pantalla inicial y la pantalla para escoger archivos txt.

---
+++ src/aplicacionia/AplicacionIA.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacionia;
+
+/**
+ *
+ * @author Martin Tepostillo
+ */
+public class AplicacionIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inicio inicio;
+        inicio = new Inicio();
+        inicio.setVisible(true);
+        
+    }
+    
+}
---
+++ src/aplicacionia/Archivo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacionia;
+
+import java.io.*;
+
+/**
+ *
+ * @author Dell
+ */
+public class Archivo {
+    
+    private String ruta_archivo;
+    
+    public Archivo(String ruta_archivo){
+        this.ruta_archivo=ruta_archivo;
+    }
+    
+    public void setRutaArchivo(){
+        
+    }
+    
+}
